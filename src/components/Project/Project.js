9d0bf95b4afd26536474de317caa2f56609a7a77
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-10-21 15:24:13 +1100

    Adding functionality to project cards so they link to Github repo and deployed sites

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,39 +1,27 @@
 
-import React, { useState } from 'react';
-import { Typography, Grid, Card, CardActionArea, CardMedia, CardContent, Grow, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+import React from 'react';
+import { Typography, Grid, Card, CardActionArea, CardMedia, CardContent, Grow } from '@mui/material';
 import './Project.css';
+import { Link } from 'react-router-dom';
 
-const ProjectCard = ({ title, caption, description, image, project, link }) => {
-
-    {/* const [projectDialog, setProjectDialog] = useState(false) */ }
+const ProjectCard = ({ title, caption, description, image, link }) => {
 
     return (
         <Grid item xs={6}>
             <Grow in timeout={1000}>
-                <Card className='custom-card'/*onClick={() => setProjectDialog(project)}*/ >
+                <Card className='custom-card'>
                     <CardActionArea>
-                        <CardMedia className='custom-card-image' image={image} title={title} />
+                        <CardMedia className='custom-card-image' image={image} title={title} links={links} />
                         <CardContent>
                             <Typography className='custom-card-title'>{title}</Typography>
                             <Typography variant='body2' className='custom-card-description'>{description}</Typography>
+                            {Object.keys(links).map(link => (
+                                <a href={link}>{link.icon}</a>
+                            ))}
                         </CardContent>
                     </CardActionArea>
                 </Card>
             </Grow>
-            {/*
-            <Dialog open={projectDialog} onClose={() => setProjectDialog(false)}>
-                <DialogTitle onClose={() => setProjectDialog(false)}>{title}</DialogTitle>
-                <img src='' alt='' />
-                <DialogContent>{description}</DialogContent>
-                <DialogActions>{links?.map((link => (
-                    <a href={link} target='_blank'>{link.icon}</a>
-                )))}</DialogActions>
-            </Dialog>
-                                            {links.map((link => (
-                                    <a href={link} >{link.icon}</a>
-                                )))}
-            */}
-
         </Grid>
     )
 }
