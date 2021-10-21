dff9c59155bf9dabcd571558a13766587f0a9638
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-10-21 19:42:21 +1100

    Completed README and added downloadable CV

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Button, Typography } from '@mui/material';
+import Resume from '../../assets/pdf/CV_Steph_Melanofridis.pdf';
 import './Button.css';
 
-const CustomButton = ({ text, endIcon, link, CV }) => {
+const CustomButton = ({ text, endIcon, link }) => {
     return (
         <div>
-            <Button className='btn' link={link} endIcon={endIcon} CV={CV}>
+            <Button className='btn' link={link} endIcon={endIcon}>
                 {link ? (
                     <Typography className='timeline-item-text'>
                         <a href={link} target='_blank'>{text}</a>
                     </Typography>) : (
-                    <Typography><a href={CV}>{text}</a></Typography>
+                    <Typography><a href={Resume} download className='download'>{text}</a></Typography>
                 )}
             </Button>
         </div>
